ee2d7c353e21b48460d9e7b36bd49e8d3f09a107
unknown <damarfc88@gmail.com>
2023-07-25 22:30:21 +0700

    CRUD JENIS - SATUAN - BARANG

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,22 @@
 <?php
 
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\JenisController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\SatuanController;
 use App\Http\Livewire\Kategori\MainIndex;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [MainController::class, 'index'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('backend')->name('backend.')->group(function () {
         Route::get('/', [MainController::class, 'main'])->name('main');
+
+        Route::resource('jenis', JenisController::class);
+        Route::resource('satuan', SatuanController::class);
+        Route::resource('barang', BarangController::class);
     });
 });
 
